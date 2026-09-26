4adbc91929a68910245691cb768e3a451c89f093
annachen8788ac-commits <annachen.8788ac@gmail.com>
2026-09-26 08:45:33 +0800

    refactor: reuse shared trade specifications in trade-rules.js

--- domain/trade-rules.js
+++ domain/trade-rules.js
@@ -1,6 +1,6 @@
 (() => {
-  const minimums = {30:200,60:1000,90:10000,180:50000,360:250000};
-  const rates = {30:21,60:29,90:37,180:45,360:53};
+  const minimums = window.DAppsTradeSpec.minimums;
+  const rates = window.DAppsTradeSpec.rates;
   const amountInput = document.querySelector('#trade-amount');
   const placeBtn = document.querySelector('#place-trade');
   const durationButtons = [...document.querySelectorAll('[data-duration]')];
